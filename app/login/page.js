ce93db7d3fb77daad3cdafde2bda8140aa9ca8d5
mriskiaziz <mriski.119140160@student.itera.ac.id>
2025-03-14 22:19:10 +0700

    update all

--- app/login/page.js
+++ app/login/page.js
@@ -44,29 +44,29 @@ export default function Page() {
 
   return (
     <div>
-      <section className="bg-slate-300 dark:bg-gray-900">
+      <section className="bg-slate-300 ">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
           >
             Welcome To Login Page
           </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="flex justify-center mb-12 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 className="flex justify-center mb-12 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                 Form Login
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSignIn}>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 ">
                     Email
                   </label>
                   <input
                     type="email"
                     name="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 "
                     placeholder="name@company.com"
                     required={true}
                     onChange={(e) => {
@@ -76,7 +76,7 @@ export default function Page() {
                   />
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 ">
                     Password
                   </label>
                   <input
@@ -84,7 +84,7 @@ export default function Page() {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 "
                     required={true}
                     onChange={(e) => {
                       setpassword(e.target.value);
@@ -98,23 +98,21 @@ export default function Page() {
                       <input
                         id="remember"
                         type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 "
                         onChange={(e) => {
                           setshowpassword(e.target.checked);
                         }}
                       />
                     </div>
                     <div className="ml-3 text-sm">
-                      <label className="text-gray-500 dark:text-gray-300">
-                        Show Password
-                      </label>
+                      <label className="text-gray-500 ">Show Password</label>
                     </div>
                   </div>
                 </div>
 
                 <button
                   type="submit"
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                   disabled={loading}
                 >
                   {!loading ? "Sign in" : "Loading..."}

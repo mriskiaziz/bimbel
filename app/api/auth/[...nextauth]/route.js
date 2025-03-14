002d7edd67d3baea4532a3d2d9edbeda16585c42
Muhammad Riski Aziz <mriski.119140160@student.itera.ac.id>
2025-03-14 14:52:46 +0700

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions = {
   session: {
@@ -18,6 +21,24 @@ export const authOptions = {
           throw new Error("Email and password required");
         }
 
+        // const user = await prisma.Akun.findUnique({
+        //   where: { email: credentials.email },
+        // });
+
+        // if (!user || !user.password) {
+        //   throw new Error("Invalid email or password");
+        // }
+
+        // const isValidPassword = await bcrypt.compare(
+        //   credentials.password,
+        //   user.password
+        // );
+
+        // if (!isValidPassword) {
+        //   throw new Error("Invalid email or password");
+        // }
+
+        // return user;
         return {
           email: "admin@gmail.com",
           role: "admin",
@@ -28,20 +49,23 @@ export const authOptions = {
       },
     }),
   ],
-  adapter: PrismaAdapter(prisma), // Pastikan PrismaAdapter digunakan
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, account }) {
+      console.log(token);
       if (user) {
         token.role = user.role;
         token.image = user.image;
-        token.accessToken = user.access_token || null;
+        token.accessToken = user.access_token;
       }
+
       return token;
     },
     async session({ session, token }) {
+      console.log(session);
       session.accessToken = token.accessToken;
       session.user.role = token.role;
       session.user.image = token.image;
+
       return session;
     },
   },
@@ -50,3 +74,4 @@ export const authOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
